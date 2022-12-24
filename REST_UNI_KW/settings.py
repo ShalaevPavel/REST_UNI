efd9568b691eb5a97286b303e939df7604026731
Pavel Shalaev <93087256+ShalaevPavel@users.noreply.github.com>
2022-12-24 23:21:44 +0300

    Update settings.py

--- REST_UNI_KW/settings.py
+++ REST_UNI_KW/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^0_)(rz47k-c)owiwn55)ahrv1)=r46q(2!9+*a*f1mlpzne-v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
